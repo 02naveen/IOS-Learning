43ba421ccb2d2835e1fc699fce6f3bd682421a08
Naveen <naveenkushwaha890@gmail.com>
2023-11-02 03:22:37 -0700

    ok

---
+++ IOS Learning/NewView.swift
@@ -0,0 +1,30 @@
+//
+//  NewView.swift
+//  IOS Learning
+//
+//  Created by Naveen kUSHWAHA on 02/11/23.
+//  Copyright © 2023 Naveen kUSHWAHA. All rights reserved.
+//
+
+import UIKit
+
+class NewView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IOS Learning/ViewController.swift
+++ IOS Learning/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        login.layer.cornerRadius = 15
+        reset.layer.cornerRadius = 15
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var login: UIButton!
+    
+    @IBOutlet weak var reset: UIButton!
+    @IBAction func reset(_ sender: Any) {
+        username.text = ""
+        password.text = ""
+    }
+    @IBAction func login(_ sender: Any) {
+        if username.text == "naveen123" && password.text == "naveen@123"
+        {
+            performSegue(withIdentifier: "login", sender: nil)
+        }
+        else {
+            let alrt = UIAlertController(title: "Wrong username or password", message: "", preferredStyle: UIAlertController.Style.alert)
+            alrt.addAction(UIAlertAction(title: "Try Again", style: UIAlertAction.Style.destructive, handler: nil))
+            present(alrt, animated: true, completion: nil)
+            username.text = ""
+            password.text = ""
+        }
+      
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
 }
+
 
